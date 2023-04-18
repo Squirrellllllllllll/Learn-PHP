c0ea9375d07980a6b9c893c364611db0e3da842a
Squirrellllllllllll <90439584+Squirrellllllllllll@users.noreply.github.com>
2023-04-18 12:54:03 +0700

    Update empty.php

--- empty.php
+++ empty.php
@@ -6,9 +6,18 @@
  * Trả về true nếu:
  *  + Không tồn tại => !isset($variable)
  *  + Rỗng, =0, trống, null, array rỗng, object rỗng (Phải ép kiểu sang array), false
+ *
+ *
+ *  Ứng dụng: Kiểm tra biến tồn tại và có dữ liệu
  */
 
 $str = "Unicode";
 
-$check = empty($str);
-var_dump($check);
+// $check = empty($str);
+// var_dump($check);
+
+if (!empty($str)) {
+    echo $str;
+} else {
+    echo "Not valiable";
+}
